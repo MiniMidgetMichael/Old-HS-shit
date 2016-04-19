abd10a706c6b7a36539c3ad5e4039971c5e746eb
MiniMidgetMichael <child2dad@gmail.com>
2016-04-19 09:07:37 -0700

    messing with stuff

---
+++ str_crypt.py
@@ -0,0 +1,34 @@
+def x_bin(s):
+    """
+        EXAMPLE:
+            'mike'
+            >>> ['01101101', '01101001', '01101011', '01100101']
+            >>> 00000100
+            >>> 01101111
+            >>> 00001010
+    """
+    ##ord('str')
+    bin_list = []
+    for i in s:
+        i = ord(i)
+        x = bin(i)
+        bin_list += [x[2:].zfill(8)]
+    print (bin_list)
+    prev_value = 0
+    value = 0
+    for index, i in enumerate(bin_list):
+        if index == 0:
+            prev_value = int(i, base=2)
+        else:
+            prev_value = value
+        i = int(i, base=2)
+        if not(index == len(bin_list) - 1):
+            value = prev_value ^ int(bin_list[index+1], base=2)
+            print (bin(value)[2:].zfill(8))
+        ##else:
+            ##print ("##", bin(i)[2:].zfill(8), "##")
+
+
+        
+
+xor = x_bin("mike")
