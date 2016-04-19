33215b6f0de8bf9ffc79bef649bbc3a9e44f0dd0
MiniMidgetMichael <child2dad@gmail.com>
2016-04-19 11:13:52 -0700

    added execution

--- str_crypt.py
+++ str_crypt.py
@@ -72,7 +72,20 @@ def and_bin(s):
 
 
 if __name__ == "__main__":
-    string = "mike"
-    print (xor_bin(string))
-    print (or_bin(string))
-    print (and_bin(string))
+    string = "dbcaefgjihklmponqrsutvwzxy"
+    """
+        >>>13 63 48
+        >>> ? 0
+    """
+    print ("## XOR ##\n" + xor_bin(string))
+    x_string = xor_bin(string)
+    print ("## OR ##\n" + or_bin(string))
+    o_string = or_bin(string)
+    print ("## AND ##\n" + and_bin(string))
+    a_string = and_bin(string)
+
+    x_value = int(int(x_string, base=2) / 2)
+    o_value = int(int(o_string, base=2) / 2)
+    a_value = int(int(a_string, base=2) / 2)
+    print (x_value, o_value, a_value)
+    print (chr(x_value), chr(o_value), chr(a_value))
