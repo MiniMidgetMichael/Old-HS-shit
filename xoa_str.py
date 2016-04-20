6df3a6c66a73d213276f6a500af28bea2e7e6d46
MiniMidgetMichael <child2dad@gmail.com>
2016-04-20 08:52:46 -0700

    re-attempting cipher methods

--- xoa_str.py
+++ xoa_str.py
@@ -74,25 +74,25 @@ def and_bin(s):
 
 
 if __name__ == "__main__":
-    ##string = "thelazybrownfoxjumpedoverthereddog1=00 1"
+    string = input("Give string: \n")
     """
         >>>13 63 48
         >>> ? 0
 
         'dbcaefgjihklmponqrsutvwzxy' = 'abcdefghijklmnopqrstuvwxyz'
-
+        M1niM1dgetMichael is a p074703!/\/\/testing happy 81r7hd4y
         ***IF CONTENTS ARE SAME, END VALUE IS SAME***
             EX.: 'mike' == 'emki'
     """
-    print ("## XOR ##\n" + xor_bin(string))
-    x_string = xor_bin(string)
-    print ("## OR ##\n" + or_bin(string))
-    o_string = or_bin(string)
-    print ("## AND ##\n" + and_bin(string))
-    a_string = and_bin(string)
-
-    x_value = int(int(x_string, base=2) / 2)
-    o_value = int(int(o_string, base=2) / 2)
-    a_value = int(int(a_string, base=2) / 2)
-    print (x_value, o_value, a_value)
-    print (chr(x_value), chr(o_value), chr(a_value))
+##    print ("## XOR ##\n" + xor_bin(string))
+##    x_string = xor_bin(string)
+##    print ("## OR ##\n" + or_bin(string))
+##    o_string = or_bin(string)
+##    print ("## AND ##\n" + and_bin(string))
+##    a_string = and_bin(string)
+##
+##    x_value = int(int(x_string, base=2) / 2)
+##    o_value = int(int(o_string, base=2) / 2)
+##    a_value = int(int(a_string, base=2) / 2)
+##    print (x_value, o_value, a_value)
+##    print (chr(x_value), chr(o_value), chr(a_value))
