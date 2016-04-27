e220a6a64f703a2ab45f7a7c1fc6c8e04dee4bac
MiniMidgetMichael <child2dad@gmail.com>
2016-04-27 08:38:20 -0700

    removed old method

--- string_cipher.py
+++ string_cipher.py
@@ -13,40 +13,6 @@ def cipher(s):
         end_values += [i]
     ciphered = "".join([chr(i) for i in end_values])
     return ciphered
-##    length = len(s)
-##    bin_list = []
-##    bin_strings = []
-##    order = {}
-##    values = {}
-##    for index, i in enumerate(s):
-##        order[index] = i
-##        values[i] = ord(i)
-##        i = ord(i)
-##        x = bin(i)
-##        bin_strings += [x[2:].zfill(8)]
-##        bin_list += [x]
-##    mixed_bin = []
-##    for i in bin_list:
-##        i = int(i, base=2)
-##        i = i ^ len(s)
-##        x = bin(i)
-##        x = x[2:].zfill(8)
-##        mixed_bin += [x]
-##    end_values = []
-##    for i in mixed_bin:
-##        i = int(i, base=2)
-##        end_values.append(i)
-##    ciphered = "".join([chr(i) for i in end_values])
-##    if __name__ == "__main__":
-##        print ("LENGTH:      ", length)
-##        print ("BIN_LIST:    ", bin_list)
-##        print ("BIN_STRINGS: ", bin_strings)
-##        print ("ORDER:       ", order)
-##        print ("VALUES:      ", values)
-##        print ("MIXED_BIN:   ", mixed_bin)
-##        print ("END_VALUES:  ", end_values)
-##        print ("CIPHERED TEXT:", ciphered)
-##    return ciphered
 
 def decipher(s):
     length = len(s)
