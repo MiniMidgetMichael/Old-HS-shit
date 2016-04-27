993d306f191b812c83ed45836b3152a82093a8c3
MiniMidgetMichael <child2dad@gmail.com>
2016-04-27 08:37:48 -0700

    cleaned cipher(s) method

--- string_cipher.py
+++ string_cipher.py
@@ -3,40 +3,53 @@
 def cipher(s):
     length = len(s)
     bin_list = []
-    bin_strings = []
-    order = {}
-    values = {}
-    for index, i in enumerate(s):
-        order[index] = i
-        values[i] = ord(i)
+    end_values = []
+    for i in s:
         i = ord(i)
         x = bin(i)
-        bin_strings += [x[2:].zfill(8)]
         bin_list += [x]
-    mixed_bin = []
     for i in bin_list:
-        i = int(i, base=2)
-        i = i ^ len(s)
-        x = bin(i)
-        x = x[2:].zfill(8)
-        mixed_bin += [x]
-    end_values = []
-    for i in mixed_bin:
-        i = int(i, base=2)
-        end_values.append(i)
+        i = int(i, base=2) ^ length
+        end_values += [i]
     ciphered = "".join([chr(i) for i in end_values])
-    if __name__ == "__main__":
-        print ("LENGTH:      ", length)
-        print ("BIN_LIST:    ", bin_list)
-        print ("BIN_STRINGS: ", bin_strings)
-        print ("ORDER:       ", order)
-        print ("VALUES:      ", values)
-        print ("MIXED_BIN:   ", mixed_bin)
-        print ("END_VALUES:  ", end_values)
-        print ("CIPHERED TEXT:", ciphered)
     return ciphered
+##    length = len(s)
+##    bin_list = []
+##    bin_strings = []
+##    order = {}
+##    values = {}
+##    for index, i in enumerate(s):
+##        order[index] = i
+##        values[i] = ord(i)
+##        i = ord(i)
+##        x = bin(i)
+##        bin_strings += [x[2:].zfill(8)]
+##        bin_list += [x]
+##    mixed_bin = []
+##    for i in bin_list:
+##        i = int(i, base=2)
+##        i = i ^ len(s)
+##        x = bin(i)
+##        x = x[2:].zfill(8)
+##        mixed_bin += [x]
+##    end_values = []
+##    for i in mixed_bin:
+##        i = int(i, base=2)
+##        end_values.append(i)
+##    ciphered = "".join([chr(i) for i in end_values])
+##    if __name__ == "__main__":
+##        print ("LENGTH:      ", length)
+##        print ("BIN_LIST:    ", bin_list)
+##        print ("BIN_STRINGS: ", bin_strings)
+##        print ("ORDER:       ", order)
+##        print ("VALUES:      ", values)
+##        print ("MIXED_BIN:   ", mixed_bin)
+##        print ("END_VALUES:  ", end_values)
+##        print ("CIPHERED TEXT:", ciphered)
+##    return ciphered
 
 def decipher(s):
+    length = len(s)
     bin_list = []
     for i in s:
         i = ord(i)
@@ -45,11 +58,9 @@ def decipher(s):
     end_values = []
     for i in bin_list:
         i = int(i, base=2)
-        i = i ^ len(s)
+        i = i ^ length
         end_values.append(i)
     deciphered = "".join([chr(i) for i in end_values])
-    if __name__ == "__main__":
-        print ("DECIPHERED:   ", deciphered)
     return deciphered
 
 
@@ -60,5 +71,8 @@ def decipher(s):
 
 if __name__ == "__main__":
     string = input("Give string: \n")
+    print ("LENGTH:       ", len(string))
     ciphered = cipher(string)
+    print ("CIPHERED:     ", ciphered)
     deciphered = decipher(ciphered)
+    print ("DECIPHERED:   ", deciphered)
