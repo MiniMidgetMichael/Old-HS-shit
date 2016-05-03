2dced5a32e4ee9394ac47f1963f79c9f7fb607f9
MiniMidgetMichael <child2dad@gmail.com>
2016-05-03 10:49:35 -0700

    Created Necessary(?) AI Files

---
+++ AI/AI.py
@@ -0,0 +1,33 @@
+#! C:/Users/MichaelLFarwell/AppData/Local/Programs/Python/Python35-32/python.exe
+import random, math
+
+"""CREATE GRID FOR AI:
+
+OOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOO
+
+[16x6]
+
+Character:
+    *
+    'spot'
+
+
+"""
+
+
+spot = '*'
+
+class grid():
+    def __init__(self, x_len, y_len):
+        
+
+        
+    self.x_len = x_len
+    self.y_len = y_len
+        
