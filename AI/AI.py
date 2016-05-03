4f1c7934d13df001ea81ab34e87bb3b2dfbdaa27
MiniMidgetMichael <child2dad@gmail.com>
2016-05-03 12:02:21 -0700

    added (slightly broken) generator for grid

--- AI/AI.py
+++ AI/AI.py
@@ -13,15 +13,9 @@ OOOOOOOOOOOOOOOO
 
 [16x6]
 
-Character:
-    *
-    'spot'
-
 
 """
 
-
-spot = '*'
 
 class grid_(object):
     def __init__(self, x_len=16, y_len=6, chr_sp='O', chr_e='*'):
@@ -46,14 +40,39 @@ class grid_(object):
             grid_x[i] = chr_sp
         for i in range(y_len):
             grid_y[i] = chr_sp
+        ##print (grid_x)
+        ##print (grid_y)
+        self.grid_x = grid_x
+        self.grid_y = grid_y
+
+    def update(self, loc, chr_):
+        assert ((type(loc) is str) and len(loc) == 2), "location must be str with a len of 2"
+        assert ((type(chr_) is int) and (chr_ == 0 or chr_ == 1)), "character must be int and 0 or 1"
+        x_len = self.x_len
+        y_len = self.y_len
+        chr_sp = self.chr_sp
+        chr_e = self.chr_e
+        grid_x = self.grid_x
+        grid_y = self.grid_y
+        x_loc = int(loc[0])
+        y_loc = int(loc[1])
+        if chr_ == 1:
+            char = chr_e
+            grid_x[x_loc] = char
+            grid_y[y_loc] = char
+        else:
+            char = chr_sp
+            grid_x[x_loc] = char
+            grid_y[y_loc] = char
         print (grid_x)
         print (grid_y)
-
-    def update(self, loc):
-        pass
+        
     
 
 grid = grid_()
+
+grid.update('01', 1)
+grid.update('12', 1)
 
 
 
