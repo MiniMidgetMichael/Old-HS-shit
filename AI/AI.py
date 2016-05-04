c9104ed65281fb2cdfe1d98c670a941b6d8d062f
MiniMidgetMichael <child2dad@gmail.com>
2016-05-04 08:45:41 -0700

    Added basic action value generator

--- AI/AI.py
+++ AI/AI.py
@@ -1,12 +1,28 @@
 #! C:/Users/MichaelLFarwell/AppData/Local/Programs/Python/Python35-32/python.exe
 import random, math, turtle, time
 
+turtle_functions = turtle._tg_turtle_functions
+
+
 class AI_(turtle.Turtle):
     def __init__(self, chance):
         assert ((type(chance) is int) and (chance > 0) and (chance < 100)), "chance must be int, and 0 < chance < 100"
         self.chance = chance
         __turtle_ = turtle.Turtle()
         self.turtle = __turtle_
+        actions = {}
+        self.actions = actions
+        self._gen_values()
+        self.actions = actions
+
+    def _gen_values(self):
+        actions = self.actions
+        freq = range(self.chance, 100)
+        if not(bool(actions)):
+            for i in turtle_functions:
+                actions[i] = random.choice(freq)
+        ##print (actions)
+            
         
 
     
@@ -15,4 +31,3 @@ class AI_(turtle.Turtle):
 
 screen = turtle.Screen()
 AI = AI_(10)
-
