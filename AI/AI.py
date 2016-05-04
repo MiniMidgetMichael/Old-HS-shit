86f4b66ec5d0f584aa42ff1aa68a3fe8bdee6321
MiniMidgetMichael <child2dad@gmail.com>
2016-05-04 08:27:35 -0700

    Added <class Turtle> generator for AI class

--- AI/AI.py
+++ AI/AI.py
@@ -1,4 +1,18 @@
 #! C:/Users/MichaelLFarwell/AppData/Local/Programs/Python/Python35-32/python.exe
-import random, math, turtle
+import random, math, turtle, time
 
+class AI_(turtle.Turtle):
+    def __init__(self, chance):
+        assert ((type(chance) is int) and (chance > 0) and (chance < 100)), "chance must be int, and 0 < chance < 100"
+        self.chance = chance
+        __turtle_ = turtle.Turtle()
+        self.turtle = __turtle_
+        
+
+    
+
+
+
+screen = turtle.Screen()
+AI = AI_(10)
 
